9341e35be40a1f8f7a753066d57a7fabbd544e42
koka-news <koka.co@gmail.com>
2025-09-24 20:16:24 +0900

    feat: initial project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
   images: {
     unoptimized: true, // GitHub Pages 不支持 Next.js 图像优化
